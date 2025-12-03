8b05dd4ed397fdeb6a9cde81d44c655256606902
Blinkflo Builder <builder@blinkflo.dev>
2025-12-03 19:24:50 +0000

    auto generated

---
+++ app/layout.jsx
@@ -0,0 +1,31 @@
+
+import "./globals.scss";
+
+export default async function RootLayout({ children }) {
+  const slug = process.env.SITE_SLUG;
+
+  const res = await fetch(
+    `https://blinkflo-backend.onrender.com/api/websites/${slug}`,
+    { cache: "no-store" }
+  );
+
+  const site = await res.json();
+
+  return (
+    <html lang="en">
+      <head>
+        <title>${site.websiteName}</title>
+        <link rel="icon" href="${site.favicon}" />
+        <style>{`
+          :root {
+            --color-lime: ${site?.colors?.[0]?.code || "#000"};
+            --color-blue-500: ${site?.colors?.[1]?.code || "#fff"};
+            --color-white: ${site?.colors?.[2]?.code || "#f00"};
+            --color-blue-400: ${site?.colors?.[2]?.code || "#f00"};
+          }
+        `}</style>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
